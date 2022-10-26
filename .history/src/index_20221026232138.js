a161eab8074cea3a95752ad70f91d1e1fbab4961
PeOmniBk <71118479+PeOmniBk@users.noreply.github.com>
2022-10-26 23:33:43 +0200

    Add redux & react-intel

--- .history/src/index_20221026232138.js
+++ .history/src/index_20221026232138.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { IntlProvider } from "react-intl";
 import { Provider } from "react-redux";
 import App from './App';
 import "./bootstrap.min.css";
@@ -11,9 +10,7 @@ import store from "./store";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <IntlProvider locale='en' defaultLocale="en">
-      <App />
-    </IntlProvider>
+    <App />
   </Provider>,
 );
 
---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { createStore } from "redux";
+
+const initialState = {};
+
+const store = createStore(
+    initialState,
+);

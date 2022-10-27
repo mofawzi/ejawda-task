fff4502168c88c1bfd8c777f668bc80bc99e6727
PeOmniBk <71118479+PeOmniBk@users.noreply.github.com>
2022-10-28 00:33:29 +0200

    Add Home screen

---
+++ .history/src/screens/HomeScreen_20221028003009.js
@@ -0,0 +1,20 @@
+import Establishment from './../components/Establishment/Establishment';
+
+const HomeScreen = () => {
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-3 p-0">
+
+                </div>
+                <div className="col-9 px-10">
+                    <main className="mt-40">
+                        <Establishment />
+                    </main>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HomeScreen;

9272cabd35db95276bc98743c992bcff6971dad5
PeOmniBk <71118479+PeOmniBk@users.noreply.github.com>
2022-10-28 23:51:48 +0200

    Update Language Handler

---
+++ .history/src/components/LanguageProvider/LanguageProvider_20221028231741.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { IntlProvider } from "react-intl";
+import { useSelector } from 'react-redux';
+import Arabic from "../../assets/language/Arabic.json";
+import English from "../../assets/language/English.json";
+
+
+const LanguageProvider = (props) => {
+    // const pageObj = document.getElementById("html");
+    // const pageObj = ReactDOM.findDOMNode("page")
+    const currentLanguage = useSelector(state => state.language);
+    const { language } = currentLanguage;
+    let messages = Arabic;
+
+    if (language.locale === "en") {
+        // pageObj.setAttribute("dir", "ltr");
+        messages = English;
+    } else if (language.locale === "ar") {
+        // pageObj.setAttribute("dir", "rtl");
+        messages = Arabic;
+    }
+
+    return (
+        <IntlProvider messages={messages} locale={language.locale} defaultLocale="en">
+            {props.children}
+        </IntlProvider>
+    );
+};
+
+export default LanguageProvider;
--- .history/src/index_20221028231300.js
+++ .history/src/index_20221028231300.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import App from './App';
-import LanguageHandler from "./components/LanguageHandler/LanguageHandler";
+import { LanguageProvider } from "./components/LanguageProvider/LanguageProvider";
 import './custom-styling.scss';
 import './index.css';
 import store from "./store";
@@ -12,9 +12,9 @@ import store from "./store";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <LanguageHandler>
+    <LanguageProvider>
       <App />
-    </LanguageHandler>
+    </LanguageProvider>
   </Provider>
 );
 
